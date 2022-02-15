1d185822623681bed180fcc9009772cd9211a04c
queuebit <queuebit10648@gmail.com>
2022-02-15 13:19:02 -0500

    Create naive implementation of computeGuess.

--- src/word-utils.test.ts
+++ src/word-utils.test.ts
@@ -1,10 +1,52 @@
 import { describe, expect, it } from "vitest";
-import { getRandomWord } from "./word-utils";
+import { computeGuess, getRandomWord, LetterState } from "./word-utils";
 import { render, screen } from "./test/test-utils";
 
-describe("word-utils", () => {
+describe("getRandomWord", () => {
   it("random word", () => {
     expect(getRandomWord()).toBeTruthy();
     expect(getRandomWord().length).toEqual(5);
+  });
+});
+
+describe("computeGuess", () => {
+  it("works with match and present", () => {
+    expect(computeGuess("boost", "basic")).toEqual([
+      LetterState.Match,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Present,
+      LetterState.Miss,
+    ]);
+  });
+
+  it("works with all matches", () => {
+    expect(computeGuess("boost", "boost")).toEqual([
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Match,
+    ]);
+  });
+
+  it("works with full miss", () => {
+    expect(computeGuess("boost", "apple")).toEqual([
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ]);
+  });
+
+  it("only does one match when two letters are present", () => {
+    expect(computeGuess("solid", "boost")).toEqual([
+      LetterState.Present,
+      LetterState.Match,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ]);
   });
 });
