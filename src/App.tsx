89f8d1f8e07fc99cbfa15bd681a2033e80f43321
queuebit <queuebit10648@gmail.com>
2022-12-01 07:34:30 -0500

    Add first guess info.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import WordRow from "./WordRow";
 
 export default function App() {
+  const [guess, setGuess] = useState("");
   return (
     <div className="mx-auto w-96">
       <header className="border-b border-gray-500 pb-2 my-2">
         <h1 className="text-4xl text-center">Spelldle</h1>
+        <div>
+          <input
+            type="text"
+            className="w-1/2 p-2 border-2 border-gray-500"
+            value={guess}
+            onChange={(e) => setGuess(e.target.value)}
+          />
+        </div>
       </header>
       <main className="grid grid-rows-6 gap-4">
         <WordRow letters="hello" />
